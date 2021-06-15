339b60b8e4f1f22a4a8a45e7273374a31117db50
Yunfan Hu <1033718596@qq.com>
2021-06-15 13:48:27 +0800

    new run shell

--- running/run.sh
+++ running/run.sh
@@ -1,7 +1,5 @@
 mkdir data result para
+cd data
+mkdir train valid
 
 CUDA_VISIBLE_DEVICES=1 python dkn_train.py
-CUDA_VISIBLE_DEVICES=1 python dkn_test.py
-
-CUDA_VISIBLE_DEVICES=1 python dkn_train_dkn.py
-CUDA_VISIBLE_DEVICES=1 python dkn_test.py
